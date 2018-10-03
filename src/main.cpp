655dc39ec086af49b4d5c6fd841e88135b6bd20c
fabulousduck <ryanvlaming@icloud.com>
2018-10-03 11:37:53 +0200

    remove old seven segment display comments

--- src/main.cpp
+++ src/main.cpp
@@ -18,26 +18,6 @@ int elevatorAtFloor = false;
 // int doorsOpen = false;
 int elevatorCalledUp = false;
 int elevatorCalledDown = false;
-/*=======================================================================================================
-//display 0,1,2,3,4,5,6,7,8
-Number 0 :  00000011     3 
-Number 1 :  10101111     159 
-Number 2 :  00100101     37
-Number 3 :  00001101     13
-Number 4 :  10011001     153
-Number 5 :  01001001     73
-Number 6 :  01000001     65
-Number 7 :  00011111     31
-Number 8 :  00000001     1
-
-To display the dot, simply subtract 1.
-example : 1 - 1 = 0  or b0000000 0x0 to display 8
-*/
-/*In order to display zero your array has decimal number 3. However using the shift register
-it is actuall the binary number that is uploaded into the resitsre: zie the table above for zero: 00000011. 
-*/
-// int datArray[9] = {3, 159, 37, 13, 153, 73, 65, 31, 1}; // array without the decimal
-
 
 //routine for when the elevator gets called up
 //currently only lights up the led. when the master code
